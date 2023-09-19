953f421d6c64ab99effea660d284871d5555e8c7
unknown <Jinbyos@Gmail.com>
2023-09-19 14:19:03 -0300

    CSharp-POO

---
+++ Desafio 1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Agregale los siguientes atributos:
+            //Modelo string.
+            //Marca string.
+            //NumeroTelefonico string.
+            //CodigoOperador int (1, 2 o 3).
+
+            string modelo;
+            string marca;
+
+            Console.Write("Ingrese el modelo de telefono: ");
+            modelo = Console.ReadLine();
+            Console.Write("Ingrese la marca: ");
+            marca = Console.ReadLine();
+
+            Telefono tel = new Telefono(modelo, marca);
+
+            Console.Write("Ingrese el numero de telefono: ");
+            tel.NumeroTelefonico = Console.ReadLine();
+            Console.Write("Ingrese el codigo operador (1,2 o 3): ");
+            tel.CodigoOperador = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("\nEl modelo es " + tel.Modelo);
+            Console.WriteLine("La marca es " + tel.Marca);
+            Console.WriteLine("El numero de telefono es " + tel.NumeroTelefonico);
+            Console.WriteLine("El codigo operador es " + tel.CodigoOperador);
+
+            Console.ReadKey();
+
+            Console.Write("\n¿Desea llamar a un contacto?\n1-Si, por favor.\n2-No, gracias.\n");
+            tel.NConfirmacion = int.Parse(Console.ReadLine());
+
+            tel.ConfirmacionLlamada();
+
+            string contacto = Console.ReadLine();
+            Console.WriteLine(tel.Llamar(contacto));
+
+            Console.ReadKey();
+            Console.WriteLine("...");
+            Console.ReadKey();
+            Console.WriteLine("...");
+            Console.ReadKey();
+            Console.WriteLine("La llamada ha fallado, intentelo nuevamente.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Desafio 1/Telefono.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_1
+{
+    internal class Telefono
+    {
+        //Agregale los siguientes atributos:
+        //Modelo string.
+        //Marca string.
+        //NumeroTelefonico string.
+        //CodigoOperador int (1, 2 o 3).
+
+        private string modelo;
+        private string marca;
+        private string numeroTelefonico;
+        private int codigoOperador;
+        private int nConfirmacion;
+
+
+        public Telefono(string modelo, string marca)
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+        }
+
+
+        public string Modelo
+        {
+            get { return modelo; }
+        }
+
+        public string Marca { get { return marca; } }
+        public string NumeroTelefonico { get; set; }
+        public int CodigoOperador
+        {
+            get { return codigoOperador; }
+            set
+            {
+                if (value >= 1 && value <= 3)
+                {
+                    codigoOperador = value;
+                }
+                else
+                {
+                    codigoOperador = 0;
+                }
+            }
+        }
+
+        public int NConfirmacion
+        {
+            set
+            {
+                if(value == 1 || value == 2)
+                {
+                    nConfirmacion = value;
+                }
+                else
+                {
+                    nConfirmacion = 2;
+                }
+            }
+        }
+
+
+        
+
+        public void ConfirmacionLlamada()
+        {
+            if (nConfirmacion == 1)
+            {
+                Console.WriteLine("Seleccione el contacto:\n1-Ana\n2-Juan\n3-Pepito\n");
+            }
+            else
+            {
+                Console.WriteLine("No se realizara la llamada a ningun contacto.");
+            }   
+
+        }
+   
+
+        public string Llamar()
+        {
+            return "Realizando llamada...";
+        }
+        public string Llamar(string contacto)
+        {
+            return "Llamando a " + contacto + "...";
+        }
+    }
+}
